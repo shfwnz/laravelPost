542780e9fee62a7c812dbb1a8c8c95b83744ec70
OyShan1 <ilhamdzaky2007@gmail.com>
2025-02-18 09:42:49 +0700

    change files

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Post;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        Post::factory()   // refers to postfactory.php class
+        ->count(20)
+        ->create(); 
+    }
+}
